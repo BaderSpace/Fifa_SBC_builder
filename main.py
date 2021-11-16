4c64180b3ccb09e1319f9c2c671235c0e24694e0
BaderSpace <baderasiri16@gmail.com>
2021-11-16 09:12:20 +0300

    Update 1: can pull club players & squads from futbin

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 from bs4 import BeautifulSoup
 import requests
 import undetected_chromedriver as uc
 import time
-import json
+import csv
+import os
 
 op = uc.ChromeOptions()
 op.add_argument('--log-level=3')
@@ -17,7 +16,6 @@ wait = WebDriverWait(driver, 50)
 
 
 def get_the_squads():
-    global r
     driver.get('https://www.futbin.com/squad-building-challenges')
     ch_name = input('Enter the challenge name: ')
     wait.until(EC.presence_of_element_located(
@@ -34,50 +32,79 @@ def get_the_squads():
                        {'style': 'text-decoration: none;'})['href'])
     time.sleep(1)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
-    driver.get('https://www.futbin.com'+soup.find('a',
-               {'class': "chal_view_btn"})['href'])
-    time.sleep(2)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    r = requests.get("https://www.futbin.com"
-                     + soup.find('a', {'class': 'squad_url'})['href'])
-    pass
+    challenge_name = str(
+        soup.find('h1', {'class': 'chal_page_name'}).text).strip()
+    subChallnge_name = soup.find_all('div', {'class': 'chal_name'})
+    create_folders(challenge_name, subChallnge_name)
+    subChallnges = soup.find_all('div', {'class': "btn_holder"})
+    for sc in subChallnges:
+        driver.get('https://www.futbin.com'
+                   + sc.findChild('a', {'class': 'chal_view_btn'})['href'])
+        time.sleep(2)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        subChNm = str(
+            soup.find('h6', {'class': 'chal_page_name'}).text).strip()
+        squads = soup.find_all('a', {'class': 'squad_url'})
+        for squad in squads:
+            r = requests.get("https://www.futbin.com" + squad['href'])
+            squad_name = str(squad.text).strip()
+            get_players_info(r, squad_name, challenge_name, subChNm)
 
 
 def get_club_players():
-    players_list = []
+    players_name = []
+    players_rate = []
+    players_pos = []
     wait.until(EC.presence_of_element_located(
         (By.CLASS_NAME, 'icon-club'))).click()
     wait.until(EC.presence_of_element_located(
         (By.CLASS_NAME, 'players-tile'))).click()
     while True:
-        wait.until(EC.presence_of_all_elements_located)
-        players = wait.until(EC.presence_of_all_elements_located(
-            (By.CLASS_NAME, 'entityContainer')))
+
+        wait.until(EC.presence_of_all_elements_located(
+            (By.CLASS_NAME, 'rating')))
+        wait.until(EC.presence_of_all_elements_located(
+            (By.CLASS_NAME, 'name')))
+        wait.until(EC.visibility_of_all_elements_located)
+        wait.until(EC.presence_of_all_elements_located(
+            (By.CLASS_NAME, 'player')))
+
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        players = soup.find_all('div', {'class': 'entityContainer'})
         for player in players:
-            try:
-                #check if loan or not
-                loan_check = player.find_element(
-                    By.CLASS_NAME, 'ut-item-player-status--loan').text
-                if len(loan_check) >= 1:
-                    continue
-                else:
-                    pass
-                web_player_name = player.find_element(
-                    By.CLASS_NAME, 'name').text
-                web_player_rating = player.find_element(
-                    By.CLASS_NAME, 'rating').text
-                players_list.append(web_player_name)
-            except:
-                continue
+            #check if loan or not
+            loan_check = player.findChild(
+                'div', {'class': 'ut-item-player-status--loan'}).text
+            if len(loan_check) == 0:
+                web_player_name = player.findChild(
+                    'div', {'class': 'name'}).text
+                web_player_rating = player.findChild(
+                    'div', {'class': 'rating'}).text
+                web_player_pos = player.findChild(
+                    'div', {'class': 'position'}).text
+                players_name.append(web_player_name)
+                players_rate.append(web_player_rating)
+                players_pos.append(web_player_pos)
+            else:
+                pass
+
         try:
             driver.find_element(By.CLASS_NAME, 'next').click()
         except:
             break
-    for i in players_list:
-        print(i)
+
+    with open('./club/club_players.csv', 'w+', encoding="utf-8", newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(['Player_name', 'Player_rate', 'Player_pos'])
+        for i in range(len(players_name)):
+            writer.writerow([players_name[i], players_rate[i], players_pos[i]])
 
 
-def get_players_info():
+def get_players_info(r, sqNm, pathName, subPathName):
+    futbin_pNms = []
+    futbin_pRts = []
+    futbin_pPos = []
+    futbin_Rpos = []
     soup = BeautifulSoup(r.text, "html.parser")
     players = soup.find_all(
         'div', {"class": 'card cardnum ui-droppable added'})
@@ -87,14 +114,28 @@ def get_players_info():
         player_rating = i.find('div', {'class': 'pcdisplay-rat'}).text
         player_pos = i.find('div', {'class': 'pcdisplay-pos'}).text
         real_pos = i['data-formpos']
-        print(player_name, player_pos, player_rating, real_pos)
+        futbin_pNms.append(player_name)
+        futbin_pRts.append(player_rating)
+        futbin_pPos.append(player_pos)
+        futbin_Rpos.append(real_pos)
+
+    with open(rf'./squads/{pathName}/{subPathName}/{sqNm}.csv', 'w+', encoding="utf-8", newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(['Player_name', 'Player_rate',
+                        'Player_pos', 'Player_form_pos'])
+        for pl in range(len(futbin_pNms)):
+            writer.writerow([futbin_pNms[pl], futbin_pRts[pl],
+                            futbin_pPos[pl], futbin_Rpos[pl]])
+        pass
 
 
 def go_to_sbc():
-    driver.find_element(By.CLASS_NAME, 'ut-tab-bar-item icon-sbc').click()
+    driver.find_element(By.CLASS_NAME, 'icon-sbc').click()
+
+#Check the players if they're availble in the club or no
 
 
-def Check_player_avalibilty():
+def Check_players_avalibilty():
     pass
 
 
@@ -111,9 +152,20 @@ def Login(usr, pas):
     driver.find_element(By.NAME, 'oneTimeCode').send_keys(Code, Keys.ENTER)
 
 
-Login('shnb_07@hotmail.com', 'Asd3546789')
-get_club_players()
-#get_the_squads()
+def create_folders(chal_name, subChal_name):
+    if not os.path.exists(rf'./squads/{chal_name}'):
+        os.mkdir(rf'./squads/{chal_name}')
+    else:
+        pass
+    for i in subChal_name:
+        if not os.path.exists(rf'./squads/{chal_name}/{str(i.text).strip()}'):
+            os.mkdir(rf'./squads/{chal_name}/{str(i.text).strip()}')
+        else:
+            pass
+
+
+#Login()
+#get_club_players()
+get_the_squads()
 #get_players_info()
-time.sleep(10)
 #driver.quit()
